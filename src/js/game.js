5cb58bbc9fdd27b46434cb916fd59489b8bed426
Вячеслав Илькевич <ivada@MacBook-Pro-Vaceslav.local>
2021-03-21 15:49:19 +0300

    Done

---
+++ src/js/game.js
@@ -0,0 +1,15 @@
+import Character from "./domain.js";
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
